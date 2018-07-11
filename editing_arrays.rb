53f05e4e2297106a308e8790a98c10ec7430c531
Uma Desai <umadesai38@gmail.com>
2018-07-11 18:13:04 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -10,5 +10,5 @@ end
 puts change_rainbow_colors
 
 def add_colors
-  # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
+ puts rainbow_colors=["red","light_red","light_yellow","green","blue"]
 end
